88f9e2e79d0c9f7b83cb6d94dccb7cdf73323cda
Bharathvaj Devarajan <bharathvajd@gmail.com>
2017-03-15 02:39:56 +0000

    Summary Statistics

---
+++ Data_Mining_Bharathvaj_Devarajan.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+df_terror <- read.csv("C:\\DCU\\Data Mining\\Mini Projects\\globalterrorismdb.csv")
+
+dim(df_terror)
+
+head(df_terror)
+
+glimpse(df_terror)
+
+summary(df_terror)
